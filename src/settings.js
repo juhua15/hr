2439bb95e05cd65dcd08a6570a722f4edc0876cf
juhua15 <1024853976@qq.com>
2022-04-12 23:15:28 +0800

    hr

---
+++ src/settings.js
@@ -0,0 +1,41 @@
+// 该文件是对项目信息进行配置，里面有三个属性
+// title 项目名称
+// fixedHeader固定头部
+// sidebarLogo 显示左侧边栏
+// setting 文件在其他文件会引用所有这里暂时不去改动
+
+module.exports = {
+  title: 'Vue Element Admin',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the settings right-panel
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component.
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: 'production'
+}
